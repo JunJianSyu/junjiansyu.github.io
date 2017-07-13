5e6d468a76d0272a468f7a6e5e912e59faa24f7d
JunJianSyu <junjian1992@gmail.com>
2017-07-13 17:25:28 +0800

    del

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -3,10 +3,6 @@ import Router from 'vue-router'
 import store from '../store'
 
 import Main from '../view/Main.vue'
-import Svux from '../view/Svux.vue'
-// Record module
-import Record from '../view/Record/Base.vue'
-import RecordIndex from '../view/Record/Index.vue'
 
 Vue.use(Router)
 const router = new Router()
@@ -14,19 +10,6 @@ const router = new Router()
 router.map({
     '/': {
         component: Main
-    },
-    '/vux': {
-        component: Svux
-    },
-    '/record': {
-        component: Record,
-        records: true,
-
-        subRoutes: {
-            '/': {
-                component: RecordIndex
-            }
-        }
     }
 })
 
